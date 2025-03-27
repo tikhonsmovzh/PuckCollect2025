8bb1fe080fa4a1c823f29b4b8b53a29fbe04c275
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-03-27 15:51:57 +0300

    feat(ElapsedTime): рестартуемое время

---
+++ include/utils/ElapsedTime.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Arduino.h>
+
+class ElapseTime{
+private:
+    uint64_t _resetMicros = 0, ;
+public:
+    ElapseTime(){
+        _resetMicros = micros();
+    }
+
+    void reset(){
+        _resetMicros = micros();
+    }
+
+    uint64_t micros(){
+        return micros() - _resetMicros;
+    }
+
+    float milliseconds(){
+        return micros() / 1000.0f;
+    }
+
+    float seconds(){
+        return micros() / 1000000.0f;
+    }
+}

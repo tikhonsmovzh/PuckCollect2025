bbae62704ecd04bd19559199a2d0f0abfb9f26e4
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-04-03 21:39:39 +0300

    fix(All): исправленое

--- include/DriveTrain.h
+++ include/DriveTrain.h
@@ -4,20 +4,11 @@
 #include "Devices.h"
 #include "Configs.h"
 
-<<<<<<< Updated upstream
-
 struct Robot{
     DcMotor* LeftWheel,* RightWheel;
     BNO055Gyro* GyroSensor;
     DistanceSensor* LeftDistSensor,* RightDistSensor,* ForwardDistSensor;
 
-=======
-struct Robot{
-    DcMotor* LeftWheel,* RightWheel;
-    BNO055Gyro* GyroSensor;
-    DistanceSensor* LeftDistSensor,* RightDistSensor,* ForwardDistSensor;
-
->>>>>>> Stashed changes
     Robot(DcMotor& LeftMotor,
     DcMotor& RightMotor,
     BNO055Gyro& GyroS,
@@ -37,13 +28,10 @@ struct Robot{
     void Drive(float forward, float turn){
         LeftWheel->setPower(forward + turn); // спасибо тежан :)
         RightWheel->setPower(forward - turn);
-<<<<<<< Updated upstream
     }
 
     float GetOriantation(){
         return GyroSensor->getOrientation().x; // надо будет свапнуть х на то что будет
-=======
->>>>>>> Stashed changes
     }
 };
 
@@ -51,30 +39,18 @@ enum Steps{
     Diagonal,
     Funnel,
     Base
-<<<<<<< Updated upstream
 };
-=======
-}
->>>>>>> Stashed changes
 
 class Driver{
 private:
     Robot* mainRobot;
 
-<<<<<<< Updated upstream
     Steps DriveSteps;
-=======
-    Steps driveSteps;
->>>>>>> Stashed changes
     float junkValues; // почему то, ни одна моя прога без них не обходится 0_о
 public:
     Driver(Robot& robot){
         mainRobot = &robot;
-<<<<<<< Updated upstream
         DriveSteps = Diagonal;
-=======
-        driveSteps = Diagonal;
->>>>>>> Stashed changes
         junkValues = 0.0f;
     }
 
@@ -82,7 +58,6 @@ public:
         switch (DriveSteps){
         case Diagonal:
             if (IS_GYRO){
-<<<<<<< Updated upstream
                 if (abs(mainRobot->GetOriantation() - 45) < ANGLE_ERROR){
                     mainRobot->Drive(0.0f, 0.0f);
                     DriveSteps = Funnel;
@@ -90,22 +65,10 @@ public:
                 }else{
                     if (mainRobot->RightWheel->getPower() == 0.0f){
                         mainRobot->Drive(0.0f, -ROBOT_SPEED);
-=======
-                switch (mainRobot.GyroS)
-                {
-                case 45:
-                    mainRobot.Drive(0.0f, 0.0f);
-                    DriveSteps = Funnel;
-                    break;
-                default:
-                    if (mainRobot.getPower == 0.0f){
-                        mainRobot.Drive(0.0f, -ROBOT_SPEED);
->>>>>>> Stashed changes
                     }
                     break;
                 }
             }else{
-<<<<<<< Updated upstream
                 if(junkValues == 0.0f){
                     mainRobot->RightWheel->softwareEncoderReset();
                     mainRobot->LeftWheel->softwareEncoderReset();
@@ -115,17 +78,6 @@ public:
                 }else{
                     if ((mainRobot->RightWheel->getCurrentPosition() + mainRobot->LeftWheel->getCurrentPosition()) * SINGLE_WHEEL_STEP_BY_ENCODER_TIC == TURN_45_DEGREE_DISTANCE){
                         mainRobot->Drive(0.0f, 0.0f);
-=======
-                if(junkValue == 0.0f){
-                    mainRobot.RightWheel.softwareEncoderReset();
-                    mainRobot.LeftWheel.softwareEncoderReset();
-                    mainRobot.Drive(0.0f, -ROBOT_SPEED);
-
-                    junkValue = 1f;
-                }else{
-                    if ((mainRobot.RightWheel.getCurrentPosition() + mainRobot.LeftWheel.getCurrentPosition()) * SINGLE_WHEEL_STEP_BY_ENCODER_TIC == TURN_45_DEGREE_DISTANCE){
-                        mainRobot.Drive(0.0f, 0.0f);
->>>>>>> Stashed changes
                         DriveSteps = Funnel;
                     }
                 }
@@ -135,11 +87,7 @@ public:
         case Funnel:
             break;
 
-<<<<<<< Updated upstream
         case Base:
-=======
-        case Base && IS_GYRO:
->>>>>>> Stashed changes
             break;
 
         default:
